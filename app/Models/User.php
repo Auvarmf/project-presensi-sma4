0ff4d8810e5a312a33ddaf34e88924eb973fbe35
kamal <ramadhan131103senin@gmail.com>
2024-01-09 10:04:38 +0700

    ubah db user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,35 +12,22 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    // protected $fillable = [
-    //     'name',
-    //     'npm',
-    //     'email',
-    //     'password',
-    // ];
+    protected $table = 'users';
 
     protected $guarded = ['id'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
+    protected $fillable = [
+        'name',
+        'nomor_induk',
+        'image',
         'password',
-        'remember_token',
+        'role'
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    protected $hidden = [
+        'password'
+    ];
+
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
